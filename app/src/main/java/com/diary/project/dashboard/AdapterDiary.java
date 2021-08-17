5682e9029343cedb5be72c8aae16090307b5aa35
naky <rahmanecky.r@gmail.com>
2021-08-17 13:41:49 +0700

    addd edit on adapterDiary

--- app/src/main/java/com/diary/project/dashboard/AdapterDiary.java
+++ app/src/main/java/com/diary/project/dashboard/AdapterDiary.java
@@ -1,6 +1,9 @@
 package com.diary.project.dashboard;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,21 +12,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.diary.project.databinding.AdapterDiaryBinding;
+import com.diary.project.databinding.EditDiaryBinding;
 import com.diary.project.models.ModelDiary;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
-public class    AdapterDiary extends RecyclerView.Adapter<AdapterDiary.AdapterHolder> {
+public class  AdapterDiary extends RecyclerView.Adapter<AdapterDiary.AdapterHolder> {
 
     Context context;
     ArrayList<ModelDiary> arrayList;
     AdapterDiaryBinding binding;
     String keyId = "";
     String date;
+    UpdateDiary diary;
 
     public AdapterDiary(Context context, ArrayList<ModelDiary> arrayList) {
         this.context = context;
         this.arrayList = arrayList;
+        diary = (UpdateDiary) context;
     }
 
     @NonNull
@@ -49,7 +57,11 @@ public class    AdapterDiary extends RecyclerView.Adapter<AdapterDiary.AdapterHo
         });
 
         holder.binding.ivEdit.setOnClickListener(v -> {
+            ModelDiary modelDiary1 = arrayList.get(position);
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            EditDiaryBinding editbinding = EditDiaryBinding.inflate(layoutInflater);
 
+            editDiaryku(modelDiary1, editbinding);
         });
 
     }
@@ -66,5 +78,43 @@ public class    AdapterDiary extends RecyclerView.Adapter<AdapterDiary.AdapterHo
             super(itemView.getRoot());
             binding = itemView;
         }
+    }
+
+    private void editDiaryku(ModelDiary md,EditDiaryBinding editDiaryBinding){
+        Dialog dialog = new Dialog(context);
+        View view = editDiaryBinding.getRoot();
+        dialog.setContentView(view);
+        dialog.show();
+
+        editDiaryBinding.title.setText(md.getTitle());
+        editDiaryBinding.description.setText(md.getDescription());
+
+        keyId = md.getUserId();
+
+        editDiaryBinding.editbt.setOnClickListener(v ->{
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy");
+            Calendar calendar = Calendar.getInstance();
+
+            date = sdf.format(calendar.getTime());
+
+            String title = editDiaryBinding.title.getText().toString();
+            String description = editDiaryBinding.description.getText().toString();
+
+            ModelDiary mdEdit = new ModelDiary(title, description, date, keyId);
+            diary.editDiary(mdEdit);
+
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+
+                    Log.d("Edit: ", "Success");
+
+                }
+            });
+        });
+    }
+
+    private  void deleteDiaryku(){
+
     }
 }
